4425cbe7eff991b0f662f746cb16a7fb31cb85db
Omkar Joshi <omkarjoshi9405@gmail.com>
2025-05-27 22:04:13 +0530

    comment added

--- main.go
+++ main.go
@@ -8,18 +8,18 @@ import (
 func odd(odd1 chan bool, even1 chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 1; i <= 49; i += 2 {
-		<-odd1           
+		<-odd1
 		fmt.Println(i)
-		even1 <- true    
+		even1 <- true
 	}
 }
 
 func even(odd1 chan bool, even1 chan bool, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for i := 2; i <= 50; i += 2 {
-		<-even1        
+		<-even1
 		fmt.Println(i)
-		odd1 <- true    
+		odd1 <- true
 	}
 }
 
@@ -27,6 +27,8 @@ func main() {
 	var wg sync.WaitGroup
 	odd1 := make(chan bool)
 	even1 := make(chan bool)
+
+	fmt.Println("channel created")
 
 	wg.Add(2)
 
@@ -36,4 +38,7 @@ func main() {
 	odd1 <- true
 
 	wg.Wait()
+
+	fmt.Println("Promgram ended")
+
 }
